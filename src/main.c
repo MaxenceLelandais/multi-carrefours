e90a04f8de009e10044507b14f1337515e21b271
Maxence Lelandais <misterduflair@gmail.com>
2023-11-13 21:15:07 +0100

    debut menu

--- src/main.c
+++ src/main.c
@@ -9,17 +9,59 @@ void carrefour(int width, int height){
     int tailleCaseX = 1;
     int tailleCaseY = 1;
 
-    draw(6,3, 0, 0, 0, 0);
-    draw(12,3, 16, 0, 0, 0);
-    draw(6,3, 39, 0, 0, 0);
+    draw(6,3, 3, 1, 0, 0);
+    draw(12,3, 19, 1, 0, 0);
+    draw(6,3, 42, 1, 0, 0);
 
-    draw(6,5, 0, 12, 0, 0);
-    draw(12,5, 16, 12, 0, 0);
-    draw(6,5, 39, 12, 0, 0);
+    draw(6,5, 3, 13, 0, 0);
+    draw(12,5, 19, 13, 0, 0);
+    draw(6,5, 42, 13, 0, 0);
 
-    draw(6,3, 0, 28, 0, 0);
-    draw(12,3, 16, 28, 0, 0);
-    draw(6,3, 39, 28, 0, 0);
+    draw(6,3, 3, 29, 0, 0);
+    draw(12,3, 19, 29, 0, 0);
+    draw(6,3, 42, 29, 0, 0);
+}
+
+void menu(int x, int y){
+    mvprintw(y - 1, x - 5, "1. Générer véhicule.");
+    mvprintw(y, x - 5, "2. Tout effacer.");
+    mvprintw(y + 1, x - 5, "3. Automatiser.");
+    refresh();
+}
+
+void generer(int x, int y){
+    mvprintw(y - 1, x - 5, "  Type véhicule.");
+    mvprintw(y, x - 5, "1. Simple.");
+    mvprintw(y + 1, x - 5, "2. Prioritaire.");
+    refresh();
+}
+
+void select(){
+    int choice;
+    // Get user input
+    while (1) {
+        choice = getch();
+        // Process the choice
+        switch (choice) {
+            case '1':
+                // Handle Option 1
+                generer(70, 17);
+                break;
+            case '2':
+                // Handle Option 2
+                mvprintw(2, 0, "You selected Option 2");
+                break;
+            case '3':
+                // Handle Option 3
+                mvprintw(4, 0, "You selected Option 3");
+                break;
+            case 'q':
+                // Quit the program if 'q' is pressed
+                endwin();
+                break;
+        }
+        refresh(); // Refresh the screen after processing user input
+    }
 }
 
 bool load(WINDOW *win){
@@ -66,13 +108,13 @@ int main() {
     // Crée une nouvelle fenêtre pour afficher le texte
     WINDOW *win = newwin(maxY, maxX, 0, 0);
     
-    
     load(win);
     // Rafraîchit la fenêtre pour afficher les modifications
     wrefresh(win);
     carrefour(1,1);
     // Attend que l'utilisateur appuie sur une touche avant de quitter
-    getch();
+    menu(70, 17);
+    select();
 
     // Nettoie les ressources ncurses et ferme le fichier
     endwin();
